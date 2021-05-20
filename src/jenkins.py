5e2296f55437097a97906a773864e113a5d7a2fb
Rajkumar <mrraj470@gmail.com>
2021-05-20 16:31:55 +0530

    Option to fetch jobs on-demand
    * added feature named 'search_starts_from' to start searching jobs from particular folder.

--- src/jenkins.py
+++ src/jenkins.py
@@ -16,11 +16,11 @@ class Config:
     DEFAULT_CATALOGUE_PREFIX = "Jenkins-"
     SECTION = "jenkins"
 
-    def __init__(self, name: str, base_url: str, folders_to_scan: list,
+    def __init__(self, name: str, base_url: str, search_starts_from: list,
                  username: str, api_token: str, searchable_as: str, enable_cache: bool):
         self.name = name
         self.base_url = base_url
-        self.folders_to_scan = folders_to_scan
+        self.search_starts_from = search_starts_from
         self.username = username
         self.api_token = api_token
         self.searchable_as = searchable_as
@@ -54,14 +54,25 @@ class Jenkins(kp.Plugin):
             ))
         self.set_catalog(suggestions)
 
-    def on_suggest(self, user_input, items_chain):
+    def on_suggest(self, user_input, items_chain: list):
         if not items_chain:
             return
         current_item = items_chain[-1]
-        target = current_item.target()
-        if target.startswith(Config.DEFAULT_CATALOGUE_PREFIX):
-            config = [config for config in self.configs if Config.DEFAULT_CATALOGUE_PREFIX + config.name == target][0]
-            self.set_suggestions(self._fetch_job_suggestion(config), kp.Match.FUZZY, kp.Sort.SCORE_DESC)
+        first_item = items_chain[0]
+        current_target = current_item.target()
+        first_target = first_item.target()
+
+        if current_target.startswith(Config.DEFAULT_CATALOGUE_PREFIX):
+            config = \
+                [config for config in self.configs if Config.DEFAULT_CATALOGUE_PREFIX + config.name == current_target][
+                    0]
+            self.set_suggestions(self._get_main_suggestions(config), kp.Match.FUZZY, kp.Sort.SCORE_DESC)
+        elif len(items_chain) > 1 and first_target.startswith(Config.DEFAULT_CATALOGUE_PREFIX):
+            config = \
+                [config for config in self.configs if Config.DEFAULT_CATALOGUE_PREFIX + config.name == first_target][
+                    0]
+            self.set_suggestions(self._get_sub_suggestions(config, current_item.short_desc()), kp.Match.FUZZY,
+                                 kp.Sort.SCORE_DESC)
 
     def on_execute(self, item, action):
         kpu.execute_default_action(self, item, action)
@@ -93,13 +104,10 @@ class Jenkins(kp.Plugin):
             if not settings.get("jenkins_base_url", section=section, fallback=""):
                 self.warn("Section '{}' doesn't have 'jenkins_base_url' defined.".format(section))
                 continue
-            if not settings.get("folders_to_scan", section=section, fallback=""):
-                self.warn("Section '{}' doesn't have 'folders_to_scan' defined.".format(section))
-                continue
             configs.append(Config(
                 section_name,
                 settings.get("jenkins_base_url", section=section, fallback="").strip("/"),
-                settings.get("folders_to_scan", section=section, fallback="").split(","),
+                settings.get("search_starts_from", section=section, fallback="/").split(","),
                 settings.get("username", section=section, fallback="").strip(),
                 settings.get("api_token", section=section, fallback="").strip(),
                 settings.get("searchable_as", section=section, fallback="").strip(),
@@ -107,13 +115,13 @@ class Jenkins(kp.Plugin):
             ))
         return configs
 
-    def _fetch_job_suggestion(self, config: Config):
+    def _get_main_suggestions(self, config: Config):
         jobs_response = []
         if self.cache.exists(config.name):
             # using cached json response
             jobs_response = self.cache.fetch_object(config.name)
         else:
-            for folder in config.folders_to_scan:
+            for folder in config.search_starts_from:
                 folder = folder.strip("/").replace("/", "/job/").strip()
                 url = "{}/job/{}/api/json?tree=jobs[name,fullName,url]".format(config.base_url, folder)
                 if not folder.strip():
@@ -126,18 +134,36 @@ class Jenkins(kp.Plugin):
             self.cache.save_object(jobs_response, config.name)
         return suggestions
 
+    def _get_sub_suggestions(self, config: Config, folder: str):
+        folder = folder.strip("/").replace("/", "/job/").strip()
+        url = "{}/job/{}/api/json?tree=jobs[name,fullName,url]".format(config.base_url, folder)
+        if not folder.strip():
+            url = "{}/api/json?tree=jobs[name,fullName,url]".format(config.base_url)
+        jobs = http_get_json(url, config.username, config.api_token)["jobs"]
+        return self._create_job_items(jobs)
+
     def _create_job_items(self, jobs: list):
         suggestions = []
         for job in jobs:
             job_type = re.sub(r".*\.", "", job["_class"])
-            suggestions.append(self.create_item(
-                category=kp.ItemCategory.URL,
-                label=job["name"],
-                short_desc=job["fullName"] + " [{}]".format(job_type),
-                target=job["url"],
-                args_hint=kp.ItemArgsHint.FORBIDDEN,
-                hit_hint=kp.ItemHitHint.IGNORE
-            ))
+            if job_type in ["FreeStyleProject", "WorkflowJob"]:
+                suggestions.append(self.create_item(
+                    category=kp.ItemCategory.URL,
+                    label=job["name"],
+                    short_desc=job["fullName"],
+                    target=job["url"],
+                    args_hint=kp.ItemArgsHint.FORBIDDEN,
+                    hit_hint=kp.ItemHitHint.IGNORE
+                ))
+            else:
+                suggestions.append(self.create_item(
+                    category=kp.ItemCategory.URL,
+                    label=job["name"] + " (Folder)",
+                    short_desc=job["fullName"],
+                    target=job["url"],
+                    args_hint=kp.ItemArgsHint.REQUIRED,
+                    hit_hint=kp.ItemHitHint.IGNORE
+                ))
         return suggestions
 
 
